191efeed3593e0d416c3b135ecabf2e8f88e0895
eu ler <27113373+lameuler@users.noreply.github.com>
2024-06-29 16:22:08 +0800

    set port number

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+    integrations: [tailwind(), svelte()],
+    server: {
+        port: 2020
+    }
 });
